e9a334f5099aec2a6fd5a2bdda131ca31e870156
ryuu <ryuuzaki.dev@gmail.com>
2024-12-12 13:09:10 -0600

    Adding a SearchBar and fixing some bugs

--- frontend/notes_challenge/src/components/Navbar/Navbar.jsx
+++ frontend/notes_challenge/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import { useState } from "react";
 
-const Navbar = () =>{
-  return(
+const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-    </div>
-  )
-}
 
-export default Navbar
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/notes_challenge/src/pages/Home/Home.jsx
+++ frontend/notes_challenge/src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return(
+    <>
+      <Navbar/>
+      <NoteCard/>
+      <div className="container mx-auto"></div>
+    </>
+  );
 };
 
 export default Home;
