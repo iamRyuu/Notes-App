bc38cafbfc3780ce96acac4a24a50be4d8c6c3df
Jorge Obando <ryuuzaki.dev@gmail.com>
2025-02-10 15:02:10 -0600

    config routing default

--- frontend/notes_challenge/src/App.jsx
+++ frontend/notes_challenge/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login/Login";
+import { Navigate } from "react-router";
 import SignUp from "./pages/SignUp/SignUp";
 
 
@@ -12,6 +13,7 @@ const routes = (
       <Route path="/dashboard" exact element={<Home/>}/>
       <Route path="/login" exact element={<Login/>}/>
       <Route path="/signup" exact element={<SignUp/>}/>
+      <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to /login by default */}
     </Routes>
   </Router>
 );
